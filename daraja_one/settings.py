42bef6871eb80aa307206e0b2036922b73c83be7
Kiprotich-Code <fleeklawi@gmail.com>
2026-01-04 23:55:39 +0300

    Classic django 'we don't know this guy' host error fix

--- daraja_one/settings.py
+++ daraja_one/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure-fs^9ywtux!kft8hub5l--7jsfrf+z_70wu9&rrc$m1rv=9k&w@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [ '*' ]
+ALLOWED_HOSTS = [ '*.verce.app', '127.0.0.1' ]
 
 
 # Application definition
